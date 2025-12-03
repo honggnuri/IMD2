52d0af024aad4f55370418db63c8deb23e9091f6
Ubuntu <ubuntu@ip-172-31-26-107.ap-southeast-2.compute.internal>
2025-12-03 22:36:52 +0000

    feat: [최종 코드 업데이트]

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require("socket.io");
+const cors = require("cors");
+const path = require('path');
+
+const app = express();
+app.use(cors());
+
+// [핵심] public 폴더를 웹사이트 메인으로 설정!
+app.use(express.static(path.join(__dirname, 'public')));
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('접속 ID:', socket.id);
+
+  socket.on('submit_flower', (data) => {
+    io.emit('to_unity', data);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('SERVER ON!');
+});
