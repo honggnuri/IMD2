23a11f9f73b4049f6e832f10a17227203114497f
hongnuri <142802392+honggnuri@users.noreply.github.com>
2025-12-11 03:34:19 +0900

    ㅇ

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require('path');
 const app = express();
 app.use(cors());
 
-// [ 핵심 ] public 폴더를 웹사이트 메인으로 설정!
+// public 폴더 정적 서빙
 app.use(express.static(path.join(__dirname, 'public')));
 
 const server = http.createServer(app);
@@ -23,10 +23,12 @@ io.on('connection', (socket) => {
   console.log('접속 ID:', socket.id);
 
   socket.on('submit_flower', (data) => {
-    io.emit('to_unity', data);
+    console.log("받은 꽃 데이터:", data);
+    io.emit('to_unity', data);   // Unity로 전달
   });
 });
 
-server.listen(3000, () => {
+// 반드시 0.0.0.0 로!!
+server.listen(3000, "0.0.0.0", () => {
   console.log('SERVER ON!');
 });
